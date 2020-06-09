3b1d04d7c8eed97e0588a52a75c1c2974088e17b
renaa <tomasrenaa@gmail.com>
2020-06-09 15:38:35 +0200

    init, add env variables

---
+++ README.md
@@ -0,0 +1,11 @@
+exellent writeup: https://www.robinwieruch.de/javascript-project-setup-tutorial/
+```
+
+npm install -g http-server
+http-server dist
+
+// add env variables
+echo 'NODE_ENV=development' > .env.development
+echo 'NODE_ENV=production' > .env.production
+
+```
---
+++ build-utils/webpack.dev.js
@@ -0,0 +1,16 @@
+// const { DefinePlugin } = require('webpack')
+const Dotenv = require('dotenv-webpack')
+module.exports = {
+  mode: 'development', 
+  devtool: 'eval-source-map',
+  plugins: [
+    // new DefinePlugin({
+    //   'process.env': {
+    //     'NODE_ENV': JSON.stringify('development')
+    //   }
+    // })
+    new Dotenv({
+      path: './.env.development'
+    })
+  ]
+}
