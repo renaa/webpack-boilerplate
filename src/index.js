46a52015cdc38f423df7f951dcc97398b5a6f095
tomas <tomasrenaa@gmail.com>
2020-06-10 18:37:55 +0200

    .

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
 import "./styles.css"
 // import sum from './x'
-debugger
+
 console.log(`Running in ${process.env.NODE_ENV} mode`)
-async function x() {
-  console.log(await sum(2, 5))
-}
 
-async function sum(a, b) {
-  setTimeout(() => {
-    return a + b
-  }, 3000)
-}
-
-x()
